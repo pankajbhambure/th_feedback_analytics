831e90920e1283df1021a286712b56f9529e447f
pankajbhambure <pankajbhambure@gmail.com>
2026-01-02 17:03:30 +0530

    Updated 20231230000017-add-api-key-auth-type.ts

--- src/migrations/20231230000017-add-api-key-auth-type.ts
+++ src/migrations/20231230000017-add-api-key-auth-type.ts
@@ -3,7 +3,17 @@ import { QueryInterface } from 'sequelize';
 export default {
   up: async (queryInterface: QueryInterface): Promise<void> => {
     await queryInterface.sequelize.query(`
-      ALTER TYPE "enum_channels_authType" ADD VALUE IF NOT EXISTS 'API_KEY';
+      DO $$
+      BEGIN
+        IF NOT EXISTS (
+          SELECT 1 FROM pg_type t
+          JOIN pg_enum e ON t.oid = e.enumtypid
+          WHERE t.typname = 'enum_channels_authType'
+          AND e.enumlabel = 'API_KEY'
+        ) THEN
+          ALTER TYPE "enum_channels_authType" ADD VALUE 'API_KEY';
+        END IF;
+      END $$;
     `);
   },
 
